468b20866dc80e350b171912153d2af975853ad5
Tejas Joshi <95303369+tejasjoshi23@users.noreply.github.com>
2023-09-12 22:53:39 +0530

    Project Added

---
+++ Assets/fantasy village/Lowpoly_Demos/Environment_Free/Helpers/MouseLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    /*
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    */
+
+    float rotationX = -0f;
+    float rotationY = -0f;
+ 
+    public Vector2 sensitivity = Vector2.one * 150f;
+ 
+    void Update()
+    {
+        rotationY += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity.x;
+        rotationX += Input.GetAxis("Mouse Y") * Time.deltaTime * -1 * sensitivity.y;
+        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+
+}
